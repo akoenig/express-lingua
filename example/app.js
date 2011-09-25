ad7fa9293d09646dba54127c5c3dd3622fae892d
André König <andre.koenig@gmail.com>
2011-09-26 01:17:55 +0200

    Version 0.0.3 from my local repo now alive on github.

---
+++ example/app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    lingua = require('lingua');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.register(".html", require("jqtpl").express);
+    app.set('views', __dirname + '/views');
+    app.set("view engine", "html");
+
+    // Lingua configuration
+    app.use(lingua(app, {
+        defaultLocale: 'de-de',
+        path: __dirname + '/i18n'
+    }));
+
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
