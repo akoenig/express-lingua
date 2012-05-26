85ea043454781353f18d5c69a8de7a602ffdcb8d
André König <andre.koenig@gmail.com>
2012-05-26 17:13:35 +0200

    Fixed app name.

--- example/app.js
+++ example/app.js
@@ -56,5 +56,5 @@ app.get('/', function(req, res) {
 });
 
 http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("lingua example app listening on port " + app.get('port'));
 });
